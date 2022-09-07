8d32cab20e23f296ae7f7e41297a79612ace89b0
zhanglistar <zhanglinuxstar@gmail.com>
2022-09-07 02:13:12 +0000

    multiple transfer and fix test cmakelists.txt

--- utils/local-engine/tool/parquet_to_mergetree.py
+++ utils/local-engine/tool/parquet_to_mergetree.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import os
+from multiprocessing import Pool
 
 parser = ArgumentParser()
 parser.add_argument("--path", type=str, required=True, help="temp directory for merge tree")
@@ -24,7 +25,7 @@ def get_transform_command(data_path,
 
 
 def get_move_command(data_path, dst_path, no):
-    return f"cp -r {data_path}/data/_local/m1/all_1_* {dst_path}/all_{no}_{no}_0"
+    return f"mkdir -p {dst_path}/all_{no}_{no}_1; cp -r {data_path}/data/_local/m1/all_1_1_1/* {dst_path}/all_{no}_{no}_1"
 
 
 def get_clean_command(data_path):
@@ -48,6 +49,40 @@ def transform(data_path, source, schema, dst):
             raise Exception(command2 + " failed")
         print(f"{abs_file}")
 
+class Engine(object):
+    def __init__(self, source, data_path, schema, dst):
+        self.source = source
+        self.data_path = data_path
+        self.schema = schema
+        self.dst = dst
+    def __call__(self, ele):
+        no = ele[0]
+        file = ele[1]
+        abs_file = f"{self.source}/{file}"
+        print(abs_file)
+        if not os.path.exists(abs_file):
+            raise f"{abs_file} not found"
+        private_path = f"{self.data_path}/{str(no)}"
+        os.system(f"mkdir -p {private_path}")
+        command1 = get_transform_command(private_path, abs_file, self.schema)
+        command2 = get_move_command(private_path, self.dst, no+1)
+        command3 = get_clean_command(private_path)
+        if os.system(command3) != 0:
+            raise Exception(command3 + " failed")
+        if os.system(command1) != 0:
+            raise Exception(command1 + " failed")
+        if os.system(command2) != 0:
+            raise Exception(command2 + " failed")
+        print(f"{abs_file}")
+
+
+def multi_transform(data_path, source, schema, dst):
+    assert os.path.exists(data_path), f"{data_path} is not exist"
+    data_inputs = enumerate([file for file in os.listdir(source) if file.endswith(".parquet")])
+    pool = Pool()
+    engine = Engine(source, data_path, schema, dst)
+    pool.map(engine, list(data_inputs))  # process data_inputs iterable with pool
+
 
 """
 python3 parquet_to_mergetree.py --path=/root/data/tmp --source=/home/ubuntu/tpch-data-sf100/lineitem --dst=/root/data/mergetree
@@ -56,4 +91,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if not os.path.exists(args.dst):
         os.mkdir(args.dst)
-    transform(args.path, args.source, args.schema, args.dst)
+    #transform(args.path, args.source, args.schema, args.dst)
+    multi_transform(args.path, args.source, args.schema, args.dst)
