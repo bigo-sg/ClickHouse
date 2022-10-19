8c76afcaea5f3c943edfaa137644ddd362d61644
lgbo-ustc <lgbo.ustc@gmail.com>
2022-10-19 10:17:30 +0800

    fixed null order

--- utils/local-engine/Parser/SerializedPlanParser.cpp
+++ utils/local-engine/Parser/SerializedPlanParser.cpp
@@ -1628,10 +1628,10 @@ DB::QueryPlanPtr SerializedPlanParser::parseSort(const substrait::SortRel & sort
 DB::SortDescription SerializedPlanParser::parseSortDescription(const substrait::SortRel & sort_rel)
 {
     static std::map<int, std::pair<int, int>> direction_map = {
-        {1, {1, 1}},
-        {2, {1, -1}},
-        {3, {-1, 1}},
-        {4, {-1, -1}}
+        {1, {1, -1}},
+        {2, {1, 1}},
+        {3, {-1, -1}},
+        {4, {-1, 1}}
     };
 
     DB::SortDescription sort_descr;
