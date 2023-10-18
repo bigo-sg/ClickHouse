5af596e3111d38094c03bb72e3ff2ee196247676
lgbo-ustc <lgbo.ustc@gmail.com>
2023-10-18 09:50:59 +0800

    fixed ut

--- tests/queries/0_stateless/02775_show_columns_called_from_clickhouse.sql
+++ tests/queries/0_stateless/02775_show_columns_called_from_clickhouse.sql
@@ -12,6 +12,7 @@ DROP TABLE IF EXISTS tab;
 
 SET allow_suspicious_low_cardinality_types=1;
 SET allow_experimental_object_type=1;
+SET enable_adaptive_reorder_short_circuit_arguments=0;
 
 CREATE TABLE tab
 (
