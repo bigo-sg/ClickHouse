f7b236649f57deeb747b91265856fc277fc244cb
lgbo-ustc <lgbo.ustc@gmail.com>
2024-01-19 11:44:50 +0800

    update test

--- tests/queries/0_stateless/02902_short_circuit_arguments_reorder_adaptively.sql
+++ tests/queries/0_stateless/02902_short_circuit_arguments_reorder_adaptively.sql
@@ -4,6 +4,8 @@ create table if not exists reorder_t1 (a UInt32, b UInt32) Engine=Memory;
 insert into reorder_t1 select number as a, number % 10 as b from numbers(512);
 insert into reorder_t1 select number as a, number % 10 as b from numbers(512);
 
+set short_circuit_function_evaluation='force_enable';
+
 select 'enable reorder arguments';
 select '-----';
 select * from reorder_t1 where a < 5 and b = 0 settings enable_adaptive_reorder_short_circuit_arguments = 1;
