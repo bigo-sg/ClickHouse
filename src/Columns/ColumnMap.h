7bbf08a73c42841a51587e756f744afb2865b02b
taiyang-li <654010905@qq.com>
2024-03-22 17:31:02 +0800

    backup

--- src/Columns/ColumnMap.h
+++ src/Columns/ColumnMap.h
@@ -71,6 +71,7 @@ public:
     void insertRangeSelective(const IColumn & src, const Selector & selector, size_t selector_start, size_t length) override;
     bool canBeInsideNullable() const override { return true; }
     ColumnPtr filter(const Filter & filt, ssize_t result_size_hint) const override;
+    void filterInPlace(const PaddedPODArray<UInt64> & indexes, size_t start) override;
     void expand(const Filter & mask, bool inverted) override;
     ColumnPtr permute(const Permutation & perm, size_t limit) const override;
     ColumnPtr index(const IColumn & indexes, size_t limit) const override;
