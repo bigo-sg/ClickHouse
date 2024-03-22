3b0fd85b2763ca9628d5ccacffcee1d1a9078e2e
taiyang-li <654010905@qq.com>
2024-03-22 18:11:03 +0800

    allmost finish

--- src/Columns/ColumnTuple.h
+++ src/Columns/ColumnTuple.h
@@ -73,6 +73,7 @@ public:
     void insertRangeSelective(const IColumn & src, const IColumn::Selector & selector, size_t selector_start, size_t length) override;
     bool canBeInsideNullable() const override { return true; }
     ColumnPtr filter(const Filter & filt, ssize_t result_size_hint) const override;
+    void filterInPlace(const PaddedPODArray<UInt64> & indexes, size_t start) override;
     void expand(const Filter & mask, bool inverted) override;
     ColumnPtr permute(const Permutation & perm, size_t limit) const override;
     ColumnPtr index(const IColumn & indexes, size_t limit) const override;
