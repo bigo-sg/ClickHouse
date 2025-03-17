b7a8fdb32c9036d11ed9a1892b4a5cb38cc1f3e0
taiyang-li <654010905@qq.com>
2025-03-17 10:16:19 +0800

    Revert "column inserter"

--- src/Processors/Merges/Algorithms/MergedData.h
+++ src/Processors/Merges/Algorithms/MergedData.h
@@ -1,101 +1,10 @@
 #pragma once
 
-#include <Columns/ColumnDecimal.h>
-#include <Columns/ColumnFixedString.h>
-#include <Columns/ColumnString.h>
-#include <Columns/ColumnsNumber.h>
-#include <Core/TypeId.h>
 #include <Processors/Chunk.h>
 #include <Processors/Merges/Algorithms/IMergingAlgorithm.h>
 
 namespace DB
 {
-
-template <TypeIndex type_index>
-struct ColumnInserter
-{
-    template <typename T>
-    explicit ColumnInserter(T value)
-    {
-        static_assert(std::is_same_v<T, uint8_t>, "Argument must be an uint8_t");
-    }
-
-#define DISPATCH_TYPE_INDEXES(M1, M2) \
-    M1(UInt8) \
-    M1(UInt16) \
-    M1(UInt32) \
-    M1(UInt64) \
-    M1(Int8) \
-    M1(Int16) \
-    M1(Int32) \
-    M1(Int64) \
-    M1(Float32) \
-    M1(Float64) \
-    M1(String) \
-    M1(FixedString) \
-    M1(BFloat16) \
-    M1(UInt128) \
-    M1(UInt256) \
-    M1(Int128) \
-    M1(Int256) \
-    M1(UUID) \
-    M1(IPv4) \
-    M1(IPv6) \
-    M2(DateTime64, ColumnDecimal<DateTime64>) \
-    M2(Decimal32, ColumnDecimal<Decimal32>) \
-    M2(Decimal64, ColumnDecimal<Decimal64>) \
-    M2(Decimal128, ColumnDecimal<Decimal128>) \
-    M2(Decimal256, ColumnDecimal<Decimal256>)
-
-
-    ALWAYS_INLINE void insertFrom(IColumn & dst, const IColumn & src, size_t n) const
-    {
-#define INSERT_FROM_WITH_COLUMN_TYPE(TYPE_INDEX, COLUMN_TYPE) \
-    if constexpr (type_index == TypeIndex::TYPE_INDEX) \
-    { \
-        auto * concrete_dst = typeid_cast<COLUMN_TYPE *>(&dst); \
-        if (concrete_dst) \
-            concrete_dst->insertFrom(src, n); \
-        else \
-            dst.insertFrom(src, n); \
-        return; \
-    }
-
-#define INSERT_FROM(TYPE_INDEX) INSERT_FROM_WITH_COLUMN_TYPE(TYPE_INDEX, Column##TYPE_INDEX)
-
-        DISPATCH_TYPE_INDEXES(INSERT_FROM, INSERT_FROM_WITH_COLUMN_TYPE)
-        dst.insertFrom(src, n);
-
-#undef INSERT_FROM
-#undef INSERT_FROM_WITH_COLUMN_TYPE
-    }
-
-    ALWAYS_INLINE void insertRangeFrom(IColumn & dst, const IColumn & src, size_t start, size_t length) const
-    {
-#define INSERT_RANGE_FROM_WITH_COLUMN_TYPE(TYPE_INDEX, COLUMN_TYPE) \
-    if constexpr (type_index == TypeIndex::TYPE_INDEX) \
-    { \
-        auto * concrete_dst = typeid_cast<COLUMN_TYPE *>(&dst); \
-        if (concrete_dst) \
-            concrete_dst->insertRangeFrom(src, start, length); \
-        else \
-            dst.insertRangeFrom(src, start, length); \
-        return; \
-    }
-
-#define INSERT_RANGE_FROM(TYPE_INDEX) INSERT_RANGE_FROM_WITH_COLUMN_TYPE(TYPE_INDEX, Column##TYPE_INDEX)
-
-        DISPATCH_TYPE_INDEXES(INSERT_RANGE_FROM, INSERT_RANGE_FROM_WITH_COLUMN_TYPE)
-        dst.insertRangeFrom(src, start, length);
-
-#undef INSERT_RANGE_FROM
-#undef INSERT_RANGE_FROM_WITH_COLUMN_TYPE
-    }
-#undef DISPATCH_TYPE_INDEXES
-};
-
-template <typename T>
-ColumnInserter(T) -> ColumnInserter<static_cast<TypeIndex>(T{})>;
 
 class Block;
 
@@ -105,9 +14,7 @@ class MergedData
 {
 public:
     explicit MergedData(bool use_average_block_size_, UInt64 max_block_size_, UInt64 max_block_size_bytes_)
-        : max_block_size(max_block_size_)
-        , max_block_size_bytes(max_block_size_bytes_)
-        , use_average_block_size(use_average_block_size_)
+        : max_block_size(max_block_size_), max_block_size_bytes(max_block_size_bytes_), use_average_block_size(use_average_block_size_)
     {
     }
 
@@ -132,10 +39,7 @@ public:
     UInt64 totalAllocatedBytes() const { return total_allocated_bytes; }
     UInt64 maxBlockSize() const { return max_block_size; }
 
-    IMergingAlgorithm::MergedStats getMergedStats() const
-    {
-        return {.bytes = total_allocated_bytes, .rows = total_merged_rows, .blocks = total_chunks};
-    }
+    IMergingAlgorithm::MergedStats getMergedStats() const { return {.bytes = total_allocated_bytes, .rows = total_merged_rows, .blocks = total_chunks}; }
 
     virtual ~MergedData() = default;
 
